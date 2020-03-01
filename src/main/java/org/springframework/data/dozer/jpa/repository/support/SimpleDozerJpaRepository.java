9e1c9d3236fe4f84b864555dbd21964d26485f67
Krasimir Chobantonov <kchobantonov@gmail.com>
2020-02-29 23:20:00 -0500

    update the class to adjust to parent class changes

--- src/main/java/org/springframework/data/dozer/jpa/repository/support/SimpleDozerJpaRepository.java
+++ src/main/java/org/springframework/data/dozer/jpa/repository/support/SimpleDozerJpaRepository.java
@@ -3,6 +3,7 @@ package org.springframework.data.dozer.jpa.repository.support;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.data.dozer.repository.support.DozerEntityInformation;
 import org.springframework.data.dozer.repository.support.SimpleDozerRepository;
+import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.dozermapper.core.Mapper;
@@ -10,9 +11,10 @@ import com.github.dozermapper.core.Mapper;
 @Transactional
 public class SimpleDozerJpaRepository<T, ID> extends SimpleDozerRepository<T, ID> {
 
-	public SimpleDozerJpaRepository(DozerEntityInformation<T, ?> entityInformation, Mapper dozerMapper,
-			String conversionServiceName, BeanFactory beanFactory) {
-		super(entityInformation, dozerMapper, conversionServiceName, beanFactory);
+	public SimpleDozerJpaRepository(RepositoryInformation repositoryInformation,
+			DozerEntityInformation<T, ?> entityInformation, Mapper dozerMapper, String conversionServiceName,
+			BeanFactory beanFactory) {
+		super(repositoryInformation, entityInformation, dozerMapper, conversionServiceName, beanFactory);
 	}
 
 }
