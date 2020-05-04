14f67060b7cb5f4b6fe23350c4c075f31cf80626
Krasimir Chobantonov <krasimir.chobantonov@vertiv.com>
2020-05-04 13:15:12 -0400

    adapt the RepositoryMetadata when searching for JPA query methods

--- src/main/java/org/springframework/data/dozer/jpa/repository/query/DozerQueryLookupStrategy.java
+++ src/main/java/org/springframework/data/dozer/jpa/repository/query/DozerQueryLookupStrategy.java
@@ -1,11 +1,17 @@
 package org.springframework.data.dozer.jpa.repository.query;
 
 import java.lang.reflect.Method;
+import java.util.Set;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.data.dozer.annotation.DozerEntity;
+import org.springframework.data.dozer.annotation.DozerRepository;
 import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.repository.core.CrudMethods;
 import org.springframework.data.repository.core.NamedQueries;
 import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.AbstractRepositoryMetadata;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.data.repository.query.RepositoryQuery;
 
@@ -29,8 +35,117 @@ public class DozerQueryLookupStrategy implements QueryLookupStrategy {
 	@Override
 	public RepositoryQuery resolveQuery(Method method, RepositoryMetadata metadata, ProjectionFactory factory,
 			NamedQueries namedQueries) {
-		return new DozerRepositoryQuery(new DozerQueryMethod(method, metadata, factory), dozerMapper,
-				adaptedQueryLookupStrategy.resolveQuery(method, metadata, factory, namedQueries), conversionServiceName,
-				beanFactory);
+
+		DozerRepository dozerRepository = AnnotatedElementUtils.findMergedAnnotation(metadata.getRepositoryInterface(),
+				DozerRepository.class);
+		if (dozerRepository != null) {
+
+			RepositoryMetadata adaptedMetadata = AbstractRepositoryMetadata
+					.getMetadata(dozerRepository.adaptedRepositoryClass());
+
+			return new DozerRepositoryQuery(new DozerQueryMethod(method, metadata, factory), dozerMapper,
+					adaptedQueryLookupStrategy.resolveQuery(method, adaptedMetadata, factory, namedQueries),
+					conversionServiceName, beanFactory);
+		}
+
+		return new DozerRepositoryQuery(
+				new DozerQueryMethod(method, metadata, factory), dozerMapper, adaptedQueryLookupStrategy
+						.resolveQuery(method, new AdaptedRepositoryMetadata(metadata), factory, namedQueries),
+				conversionServiceName, beanFactory);
+
+	}
+
+	private class AdaptedRepositoryMetadata implements RepositoryMetadata {
+		private RepositoryMetadata delegate;
+
+		public AdaptedRepositoryMetadata(RepositoryMetadata delegate) {
+			this.delegate = delegate;
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getIdType()
+		 */
+		public Class<?> getIdType() {
+			return delegate.getIdType();
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getDomainType()
+		 */
+		public Class<?> getDomainType() {
+			Class<?> domainType = delegate.getDomainType();
+
+			DozerEntity dozerEntity = AnnotatedElementUtils.findMergedAnnotation(domainType, DozerEntity.class);
+			if (dozerEntity != null) {
+				return dozerEntity.adaptedDomainClass();
+			}
+			return domainType;
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getRepositoryInterface()
+		 */
+		public Class<?> getRepositoryInterface() {
+			Class<?> repositoryInterface = delegate.getRepositoryInterface();
+
+			DozerRepository dozerRepository = AnnotatedElementUtils.findMergedAnnotation(repositoryInterface,
+					DozerRepository.class);
+			if (dozerRepository != null) {
+				return dozerRepository.adaptedRepositoryClass();
+			}
+
+			return repositoryInterface;
+		}
+
+		/**
+		 * @param method
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getReturnedDomainClass(java.lang.reflect.Method)
+		 */
+		public Class<?> getReturnedDomainClass(Method method) {
+			Class<?> returnedDomainClass = delegate.getReturnedDomainClass(method);
+			DozerEntity dozerEntity = AnnotatedElementUtils.findMergedAnnotation(returnedDomainClass,
+					DozerEntity.class);
+			if (dozerEntity != null) {
+				return dozerEntity.adaptedDomainClass();
+			}
+			return returnedDomainClass;
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getCrudMethods()
+		 */
+		public CrudMethods getCrudMethods() {
+			return delegate.getCrudMethods();
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#isPagingRepository()
+		 */
+		public boolean isPagingRepository() {
+			return delegate.isPagingRepository();
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#getAlternativeDomainTypes()
+		 */
+		public Set<Class<?>> getAlternativeDomainTypes() {
+			return delegate.getAlternativeDomainTypes();
+		}
+
+		/**
+		 * @return
+		 * @see org.springframework.data.repository.core.RepositoryMetadata#isReactiveRepository()
+		 */
+		public boolean isReactiveRepository() {
+			return delegate.isReactiveRepository();
+		}
+
 	}
 }
