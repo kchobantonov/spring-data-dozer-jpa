851953e2e7fe41277bedbe555688a0f14a035aef
Krasimir Chobantonov <krasimir.chobantonov@vertiv.com>
2020-05-08 16:27:41 -0400

    make sure that all methods are transactional

--- src/main/java/org/springframework/data/dozer/jpa/repository/support/SimpleDozerJpaRepository.java
+++ src/main/java/org/springframework/data/dozer/jpa/repository/support/SimpleDozerJpaRepository.java
@@ -1,6 +1,11 @@
 package org.springframework.data.dozer.jpa.repository.support;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.dozer.repository.support.DozerEntityInformation;
 import org.springframework.data.dozer.repository.support.SimpleDozerRepository;
 import org.springframework.data.repository.core.RepositoryInformation;
@@ -15,6 +20,71 @@ public class SimpleDozerJpaRepository<T, ID> extends SimpleDozerRepository<T, ID
 			DozerEntityInformation<T, ?> entityInformation, Mapper dozerMapper, String conversionServiceName,
 			BeanFactory beanFactory) {
 		super(repositoryInformation, entityInformation, dozerMapper, conversionServiceName, beanFactory);
+	}
+
+	@Override
+	public Iterable<T> findAll(Sort sort) {
+		return super.findAll(sort);
+	}
+
+	@Override
+	public Page<T> findAll(Pageable pageable) {
+		return super.findAll(pageable);
+	}
+
+	@Override
+	public <S extends T> S save(S resource) {
+		return super.save(resource);
+	}
+
+	@Override
+	public <S extends T> Iterable<S> saveAll(Iterable<S> resources) {
+		return super.saveAll(resources);
+	}
+
+	@Override
+	public Optional<T> findById(ID resourceId) {
+		return super.findById(resourceId);
+	}
+
+	@Override
+	public boolean existsById(ID resourceId) {
+		return super.existsById(resourceId);
+	}
+
+	@Override
+	public Iterable<T> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Iterable<T> findAllById(Iterable<ID> resourceIds) {
+		return super.findAllById(resourceIds);
+	}
+
+	@Override
+	public long count() {
+		return super.count();
+	}
+
+	@Override
+	public void deleteById(ID resourceId) {
+		super.deleteById(resourceId);
+	}
+
+	@Override
+	public void delete(T resource) {
+		super.delete(resource);
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends T> resources) {
+		super.deleteAll(resources);
+	}
+
+	@Override
+	public void deleteAll() {
+		super.deleteAll();
 	}
 
 }
